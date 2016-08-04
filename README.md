228f03095c78652889e861eddd82756dfde6511d
Steve.King <mail@steveking.info>
2016-08-04 11:54:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Github Battle
+
+The result of following the brilliant 'React JS Fundamentals' tutorial at [reactjsprogram.com](http://www.reactjsprogram.com/)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: './app/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+
+module.exports = {
+	entry: [
+		'./app/index.js'
+	],
+	output: {
+		path: './dist',
+		filename: 'index_bundle.js'
+	},
+	module: {
+		loaders: [
+			{test: [/\.js$/, /\.jsx$/], exclude: /node_modules/, loader: 'babel-loader'}
+		]
+	},
+	plugins: [HtmlWebpackPluginConfig],
+	devtool: 'source-map',
+	devServer: {
+		stats: 'errors-only',
+	}
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	}
+};
